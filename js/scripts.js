88940d8e689515508ea0bb0d16c6f63ff36d8403
dusty mccord and Patrick Kille <dusty@>
2020-01-14 09:18:24 -0800

    add a priate, pirate cat, and a cool pro ninja.

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    $('.clickable').click(function(){
+        $('#walrus-showing').toggle();
+        $('#walrus-hidden').toggle();
+    });
+
+    $('.pi-toggle').click(function(){
+        $('#thispirate').slideToggle();
+        $('#thiscat').slideToggle();
+    });
+
+    $('#this-ninja').click(function(){
+        $('#pro-ninja').fadeToggle();
+    });
+
+
+  });
